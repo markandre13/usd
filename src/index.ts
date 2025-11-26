b388c207eb413f3ed48c6a823fadb193a29a616e
Mark-André Hopf <mhopf@mark13.org>
2025-11-26 07:52:50 +0100

    begin to add geometry classes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { Path } from "./path/Path.ts"
 import { Reader } from "./crate/Reader.js"
 import { CrateFile } from "./crate/CrateFile.ts"
 import { UsdNode } from "./crate/UsdNode.ts"
+import { vec3 } from "gl-matrix"
 
 type Index = number
 export type StringIndex = Index
@@ -142,7 +143,7 @@ export class UsdStage {
         const pathSegments = path.split('/').splice(1)
         // console.log(`TRAVERSE %o (${path})`, s)
         let node: UsdNode | undefined = this._crate._nodes[0]
-        for(const segment of pathSegments) {
+        for (const segment of pathSegments) {
             if (node === undefined) {
                 throw Error(`path '${path}' not found`)
             }
@@ -152,7 +153,7 @@ export class UsdStage {
             // console.log(`FOUND ${i.name}, LOOKUP '${t}'`)
             node = node.getChildPrim(segment)
         }
-        
+
         // if (i !== undefined) {
         //     console.log(`FOUND NOW %o`, i.name)
         // } else {
@@ -160,4 +161,103 @@ export class UsdStage {
         // }
         return node
     }
+}
+
+// OpenUSD/pxr/usd/usd/schema.usda:class "Typed"
+// OpenUSD/pxr/usd/usd/typed.h
+
+/**
+ * The base class for all _typed_ schemas (those that can impart a
+ * typeName to a UsdPrim), and therefore the base class for all
+ * concrete, instantiable "IsA" schemas.
+ *      
+ * UsdTyped implements a typeName-based query for its override of
+ * UsdSchemaBase::_IsCompatible().  It provides no other behavior.
+ */
+class Typed {
+
+}
+
+// OpenUSD/pxr/usd/usdGeom/schema.usda
+
+/**
+ * Base class for all prims that may require rendering or 
+ * visualization of some sort. The primary attributes of Imageable 
+ * are _visibility_ and _purpose_, which each provide instructions for
+ * what geometry should be included for processing by rendering and other
+ * computations.
+ */
+class Imageable extends Typed {
+
+}
+
+/**
+ * Base class for all transformable prims, which allows arbitrary
+ * sequences of component affine transformations to be encoded.
+ */
+class Xformable extends Imageable {
+
+}
+
+/**
+ * Concrete prim schema for a transform, which implements Xformable
+ */
+class XForm extends Xformable {
+}
+
+/**
+ * Boundable introduces the ability for a prim to persistently
+ * cache a rectilinear, local-space, extent.
+ * 
+ * ### Why Extent and not Bounds ?
+ * Boundable introduces the notion of "extent", which is a cached computation
+ * of a prim's local-space 3D range for its resolved attributes <b>at the
+ * layer and time in which extent is authored</b>.  We have found that with
+ * composed scene description, attempting to cache pre-computed bounds at
+ * interior prims in a scene graph is very fragile, given the ease with which
+ * one can author a single attribute in a stronger layer that can invalidate
+ * many authored caches - or with which a re-published, referenced asset can
+ * do the same.
+ *   
+ * Therefore, we limit to precomputing (generally) leaf-prim extent, which
+ * avoids the need to read in large point arrays to compute bounds, and
+ * provides UsdGeomBBoxCache the means to efficiently compute and
+ * (session-only) cache intermediate bounds.  You are free to compute and
+ * author intermediate bounds into your scenes, of course, which may work
+ * well if you have sufficient locks on your pipeline to guarantee that once
+ * authored, the geometry and transforms upon which they are based will
+ * remain unchanged, or if accuracy of the bounds is not an ironclad
+ * requisite. 
+ *   
+ * When intermediate bounds are authored on Boundable parents, the child prims
+ * will be pruned from BBox computation; the authored extent is expected to
+ * incorporate all child bounds.
+ */
+class Boundable extends Xformable {
+    extent!: vec3[]
+}
+
+/**
+ * Base class for all geometric primitives.  
+ *   
+ * Gprim encodes basic graphical properties such as \\em doubleSided and
+ * \\em orientation, and provides primvars for "display color" and "display
+ * opacity" that travel with geometry to be used as shader overrides.
+ */
+class Gprim extends Boundable {
+
+}
+
+/**
+ * Base class for all UsdGeomGprims that possess points,
+ * providing common attributes such as normals and velocities.
+ */
+class PointBased extends Gprim {
+    points?: ArrayLike<number>
+    normals?: ArrayLike<number>
+}
+
+class Mesh extends PointBased {
+    faceVertexIndices!: ArrayLike<number>
+    faceVertexCounts!: ArrayLike<number>
 }
